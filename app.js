af56ff8767617cecf9f65063dd87e64e7fcc1fb2
gitcrackerUK <pfsiwek@gmail.com>
2020-05-30 00:25:30 +0100

    front page layout done

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-let q = 'spaceX astronauts'
+function createArticle(query,article,elemntId,big){
+    axios.get(`https://images-api.nasa.gov/search?q=${query}&media_type=image`).then((res) => {
+        let imgLink = res.data.collection.items[article].links[0].href
+        let description = res.data.collection.items[article].data[0].description;
+        let title = res.data.collection.items[article].data[0].title;
+        const trump = document.querySelector(`#${elemntId}`)
+        let img = document.createElement('img')
+        let h4 = document.createElement('h4')
+        let p = document.createElement('p')
+        trump.appendChild(img)
+        trump.appendChild(h4)
+        trump.appendChild(p)
+        img.setAttribute('src', imgLink)
+        img.setAttribute('class', 'round')
+        img.setAttribute('class', 'imgArticle')
+        h4.setAttribute('class', 'h4Article')
+        p.setAttribute('class', 'pArticle')
+        h4.innerText = title;
+        if(!big){
+            p.innerText = description
+        }else{
+            img.setAttribute('class','big')
+        h4.setAttribute('class', 'bigTitle')
+            
+            p.innerText = '';
+            
+        }
+        
+     })
+}
 
-axios.get(`https://images-api.nasa.gov/search?q=${q}&media_type=image`).then((res) => {
-    let article = 1
-    let imgLink = res.data.collection.items[article].links[0].href
-    let description = res.data.collection.items[article].data[0].description;
-    let title = res.data.collection.items[article].data[0].title;
-    const spaceX = document.querySelector('#spacex');
-    let img = document.createElement('img')
-    let h3 =document.createElement('h4')
-    let p =document.createElement('p')
-    spaceX.appendChild(img)
-    spaceX.appendChild(h3)
-    spaceX.appendChild(p)
-    img.setAttribute('src', imgLink)
-    h3.innerText=title;
-    p.innerText=description
-})
 
-// data.collection.items
+//createarticle
+//serach for
+//index of array in result object 
+//div id
+
+createArticle('Trump',14,'trump', false)
+createArticle('SpaceX astronauts',1,'spacex',false)
+createArticle('sun',2,'sun',true)
+createArticle('Earth',6,'epic',false)
+createArticle('Moon ',53,'moon',false)
