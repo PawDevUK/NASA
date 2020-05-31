3339f968e8550201e7ec68728e49c6a992171da7
gitcrackerUK <pfsiwek@gmail.com>
2020-05-31 14:01:51 +0100

    Dynamicly generated rows and random articles

--- app.js
+++ app.js
@@ -1,43 +1,82 @@
-function createArticle(query, article, elemntId, big) {
+function bootstrapRow(query, article) {
     axios.get(`https://images-api.nasa.gov/search?q=${query}&media_type=image`).then((res) => {
-        let imgLink = res.data.collection.items[article].links[0].href
-        let description = res.data.collection.items[article].data[0].description;
-        let title = res.data.collection.items[article].data[0].title;
-        const element = document.querySelector(`#${elemntId}`)
-        let img = document.createElement('img')
-        let h4 = document.createElement('h4')
-        let p = document.createElement('p')
-        element.appendChild(img)
-        element.appendChild(h4)
-        element.appendChild(p)
-        img.setAttribute('src', imgLink)
-        // img.setAttribute('class', 'round')
-        img.setAttribute('class', 'imgArticle')
-        h4.setAttribute('class', 'h4Article')
-        p.setAttribute('class', 'pArticle')
-        h4.innerText = title;
-        if (!big) {
-            p.innerText = description
+        console.log(res)
+       
+        const element = document.querySelector(`article`)
+        let row = document.createElement('div')
+        row.setAttribute('class', 'row')
+        element.appendChild(row)
+
+        let cardArticle = function (width) {
+            let art = Math.floor(Math.random() * 25) + 1;
+            const resDat = res.data.collection.items[art]
+            let title = resDat.data[0].title;
+            let imgLink = resDat.links[0].href
+    
+            let card = document.createElement('div')
+            let cardBody = document.createElement('div')
+            let img = document.createElement('img')
+            let h5 = document.createElement('h5')
+
+            row.appendChild(card)
+            card.appendChild(img)
+            card.appendChild(cardBody)
+            cardBody.appendChild(h5)
+            card.setAttribute('class', 'card')
+            card.classList.add('class', `col-${width}`)
+            card.classList.add('class', `mx-auto`)
+            row.classList.add('class', `p-2`)
+            img.setAttribute('src', imgLink)
+            img.setAttribute('class', 'card-img-top')
+            cardBody.setAttribute('class', 'card-body')
+            h5.innerText = title;
+        }
+
+        let rowLayoutThree = [
+            [3, 5, 3],
+            [5, 3, 3],
+            [3, 3, 5],
+            [3, 4, 4],
+            [3, 4, 3],
+            [4, 3, 3],
+            [4, 4, 3],
+        ]
+
+        const rowLayoutTwo = [
+            [6, 5],
+            [5, 6],
+            [8, 3],
+            [3, 8],
+        ]
+        const randThree = Math.floor(Math.random() * 6) + 1
+        const randTwo = Math.floor(Math.random() * 3) + 1;
+        // console.log(randTwo);
+        // console.log(randThree);
+        // console.log('three');
+        // console.log(rowLayoutThree[randThree][0]);
+        // console.log(rowLayoutThree[randThree][1]);
+        // console.log(rowLayoutThree[randThree][2]);
+        // console.log('two');
+        // console.log(rowLayoutTwo[randTwo][0]);
+        // console.log(rowLayoutTwo[randTwo][1]);
+
+        const ran = Math.random() >= 0.5
+        if (ran) {
+            cardArticle(rowLayoutThree[randThree][0])
+            cardArticle(rowLayoutThree[randThree][1])
+            cardArticle(rowLayoutThree[randThree][2])
         } else {
-            img.setAttribute('class', 'big')
-            h4.setAttribute('class', 'bigTitle')
-            p.innerText = '';
+            cardArticle(6)
+            cardArticle(5)
         }
 
     })
 }
 
+bootstrapRow('Trump')
+bootstrapRow('SpaceX' )
+bootstrapRow('Dragon' )
+bootstrapRow('Moon' )
+bootstrapRow('Sun' )
 
-//createarticle
-//serach for
-//index of array in result object 
-//div id
-
-createArticle('Trump 2020', 1, 'trump', false) //14
-createArticle('SpaceX astronauts', 1, 'spacex', false)
-createArticle('sun', 2, 'sun', true)
-createArticle('Earth', 13, 'epic', false)
-createArticle('Moon', 29, 'moon', false)
-createArticle('Moon', 22, 'moon2', false)
-createArticle('Jupiter', 23, 'jupiter', true)
-createArticle('Mars', 08, 'mars', true)
+// TODO adjust card body 
