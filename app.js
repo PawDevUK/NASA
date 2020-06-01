cc2e741e37ff6a0987854a07951ebc9368c7c9d8
gitcrackerUK <pfsiwek@gmail.com>
2020-06-01 23:34:14 +0100

    displayMainNone() navbarActive()

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ function bootstrapRow(query) {
         let row = document.createElement('div')
         row.setAttribute('class', 'row')
         element.appendChild(row)
-        
+
         let cardArticle = function (width) {
             let art = Math.floor(Math.random() * 40) + 1;
             const resDat = res.data.collection.items[art];
 
             let title = resDat.data[0].title;
             let imgLink = resDat.links[0].href
-            
+
 
             let card = document.createElement('div')
             let cardBody = document.createElement('div')
@@ -25,19 +25,19 @@ function bootstrapRow(query) {
             card.setAttribute('class', 'card')
             card.setAttribute('data-toggle', 'modal')
             card.setAttribute('data-target', '#staticBackdrop')
-        
+
 
             const modalBody = document.querySelector('#modalBody')
             const modalTitle = document.querySelector('#staticBackdropLabel')
             const staticBackdrop = document.querySelector('#staticBackdrop')
 
-            card.addEventListener('click',()=>{
+            card.addEventListener('click', () => {
                 modalBody.innerText = resDat.data[0].description;
                 modalTitle.innerText = title
                 let modalImg = document.createElement('img')
                 modalBody.appendChild(modalImg)
-                modalImg.setAttribute('src',imgLink)
-                modalImg.setAttribute('id','modalImg')
+                modalImg.setAttribute('src', imgLink)
+                modalImg.setAttribute('id', 'modalImg')
             })
 
             card.classList.add(`col-${width}`)
@@ -47,7 +47,7 @@ function bootstrapRow(query) {
             img.setAttribute('class', 'card-img-top')
             cardBody.setAttribute('class', 'card-body')
             h5.innerText = title;
-          
+
         }
 
         let rowLayoutThree = [
@@ -67,7 +67,7 @@ function bootstrapRow(query) {
             [3, 8],
         ]
         const randThree = Math.floor(Math.random() * 6) + 1
-        
+
         const randTwo = Math.floor(Math.random() * 3) + 1;
         const ran = Math.random() >= 0.5
         if (ran) {
@@ -89,20 +89,26 @@ bootstrapRow('Sun')
 bootstrapRow('2020')
 bootstrapRow('Astronauts')
 
-const AllLi = document.querySelectorAll('.nav-item')
-for(let li of AllLi){
-    li.addEventListener('click',function(){
-        for(let li of AllLi ){
-            li.classList.remove('active')
-        }
-        this.classList.add('active')
-    })
+function navbarActive(){
+    const AllLi = document.querySelectorAll('.nav-item')
+    for (let li of AllLi) {
+        li.addEventListener('click', function () {
+            for (let li of AllLi) {
+                li.classList.remove('active')
+            }
+            this.classList.add('active')
+        })
+    }
 }
 
-
-const spacex = AllLi[0]
-const main = document.querySelector('main')
-main.style.display = 'none'
-spacex.addEventListener('click', ()=>{
-if()
-})
+function displayMainNone() {
+    const AllLi = document.querySelectorAll('.nav-item')
+    const main = document.querySelector('main')
+    for (let li of AllLi) {
+        li.addEventListener('click', (e) => {
+            main.style.display = (main.style.display) ? '' : 'none'
+        })
+    }
+}
+displayMainNone()
+navbarActive()
